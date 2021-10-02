52614293216304d94326936cd49ea48c44685606
Eduardo Piñero <81439279+edpe2003@users.noreply.github.com>
2021-10-02 19:00:37 -0300

    Delete Tienda directory

--- Tienda/controlador/VerFechaPedido.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include('../modelo/Pedido.php');
-$pedido = new Pedido();
-$fecha_1= $_POST['fecha_1'];
-$fecha_2= $_POST['fecha_2'];
-include('../vista/templateFechaPedido.php');
--- Tienda/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-include('vista/templateTienda.php');
